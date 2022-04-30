049755e2f52e454d409d1bac006d1b7109bebbb9
OmarHamdann <omar.haamdan@gmail.com>
2022-04-30 22:12:54 +0300

    oddsVsEvens

--- Round_1/binToOct/index.js
+++ Round_1/binToOct/index.js
@@ -1,4 +1,41 @@
-function octToBin(octal) {
-    return parseInt(octal, 8).toString(2);
-}
-    console.log(octToBin(1000));
+function oddsVsEvens(num) {
+    let stringNum = num.toString();
+   let numArr=stringNum.split('');
+    var odds = [];
+    var evens = [];
+    let oddSum = 0;
+    let evenSum = 0;
+
+    for (var i = 0; i < numArr.length; i++) {
+        if (numArr[i] % 2 === 0) {
+            evens.push(numArr[i]);
+        }
+        else {
+            odds.push(numArr[i]);
+        }
+    }
+
+    for (var i = 0; i < odds.length; i++) {
+        oddSum += Number(odds[i]);
+    }
+
+    for (var i = 0; i < evens.length; i++) {
+        evenSum += Number(evens[i]);
+    }
+
+   if (oddSum > evenSum) {
+       return 'odd';
+   }
+    else if (oddSum <evenSum) {
+        return 'even';
+    }
+    else {
+        return 'equal';
+    }
+
+       
+   }
+
+    
+  
+  console.log(oddsVsEvens(463));
